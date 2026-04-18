7fcc0a0b5b7121a513ee91e0facfb27d59570aa4
ShcwabSergey <sergey.shbab@gmail.com>
2026-04-18 23:53:26 +0300

    feat: add environment-based configuration and custom dev server port

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const viteEnv: Record<string, string> = loadEnv(mode, process.cwd(), '');
+  const devServerPort: number = Number(viteEnv.VITE_DEV_SERVER_PORT || '5173');
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    server: {
+      port: devServerPort,
+    },
+  };
 });
